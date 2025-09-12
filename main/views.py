156d0c40f60e30d15138d0f52d307770715c59b0
AsukaOurijya <azkaawliya06@gmail.com>
2025-09-12 16:43:19 +0700

    few fixes on views.py

--- main/views.py
+++ main/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from main.forms import NewsForm 
 from main.models import News
 from django.http import HttpResponse
 from django.core import serializers
+from .forms import NewsForm
 
 #Views
 
